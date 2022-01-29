7e703ba6cec18ea14859fe25ade5dd74d0133037
dejuny <dejun.yang0501@gmail.com>
2022-01-29 11:05:27 -0500

    This is the first version

---
+++ main.py
@@ -0,0 +1,10 @@
+def main_wrapper():
+    print(f"This is the start of our python project. This function's name is {main_wrapper.__name__} ")
+
+    # code here
+
+    print("This is the end of our python project.")
+
+
+if __name__ == "__main__":
+    main_wrapper()
